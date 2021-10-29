887d420f55feec461a9ff5fdaa60f188b98f3258
Konstantin Varlamov <konstantin.varlamov@arcadia.spb.ru>
2021-10-29 17:17:16 +0300

    Getting access to element in Right Angular way

--- angularDirectives/src/app/Directives/style.directive.ts
+++ angularDirectives/src/app/Directives/style.directive.ts
@@ -1,9 +1,11 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[appStyle]'
 })
 
 export class StyleDirective {
-
+    constructor(private el: ElementRef, private render: Renderer2) {
+        this.render.setStyle(this.el.nativeElement, 'color', 'blue')
+    }
 }
