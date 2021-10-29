ad4989c289dd1a54382d2700df34c69df3690c45
Konstantin Varlamov <konstantin.varlamov@arcadia.spb.ru>
2021-10-29 22:57:09 +0300

    Custom pipes

--- angularPipes/src/app/app.module.ts
+++ angularPipes/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MultByPipe } from './Pipes/mult-by.pipe';
+import { ExMarksPipe } from './Pipes/ex-marks.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultByPipe,
+    ExMarksPipe
   ],
   imports: [
     BrowserModule,
