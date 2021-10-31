316fbcc178350f7e2099cb16ef5fec98ab6e7b6d
Konstantin Varlamov <konstantin.varlamov@arcadia.spb.ru>
2021-10-31 17:17:05 +0300

    Service creation

--- angularServices/src/app/app.module.ts
+++ angularServices/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppCounterService } from './services/app-counter.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AppCounterService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
