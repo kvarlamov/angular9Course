d691aab28ee92cdf3cf173fd356704b84abf505f
Konstantin Varlamov <konstantin.varlamov@arcadia.spb.ru>
2021-10-31 17:45:26 +0300

    Local and Global area of service visibility

--- angularServices/src/app/app.module.ts
+++ angularServices/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppCounterService } from './services/app-counter.service';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
