bc58c1aaa5f30d920d80908a9dd37dd1d17469c8
Konstantin Varlamov <konstantin.varlamov@arcadia.spb.ru>
2021-11-01 12:40:22 +0300

    First get request

---
+++ angularHTTP/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+export interface Todo {
+  completed: boolean,
+  title: string,
+  id?: number
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {  
+
+  todos: Todo[] = []
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(todos => {
+        this.todos = todos
+      })
+  }
+}
