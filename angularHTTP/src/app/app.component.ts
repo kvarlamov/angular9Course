e24f6773ef250f09c65e3c3029fd63ec4b3c5b29
Konstantin Varlamov <konstantin.varlamov@arcadia.spb.ru>
2021-11-01 12:47:56 +0300

    Post request

--- angularHTTP/src/app/app.component.ts
+++ angularHTTP/src/app/app.component.ts
@@ -15,6 +15,7 @@ export interface Todo {
 export class AppComponent implements OnInit {  
 
   todos: Todo[] = []
+  todoTitle = ''
 
   constructor(private http:HttpClient) {}
 
@@ -22,6 +23,24 @@ export class AppComponent implements OnInit {
     this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
       .subscribe(todos => {
         this.todos = todos
+      })
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return
+    }
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        console.log(todo);
+        this.todos.push(todo);
+        this.todoTitle = ''
       })
   }
 }
