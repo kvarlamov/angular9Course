4273bbabd7abb21ccb90c93eedb3d391801e80b7
Konstantin Varlamov <konstantin.varlamov@arcadia.spb.ru>
2021-11-03 10:07:44 +0300

    adding options to post: headers

--- angularHTTP/src/app/todos.service.ts
+++ angularHTTP/src/app/todos.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, pipe, throwError } from "rxjs";
 import { catchError, delay } from "rxjs/operators";
@@ -14,11 +14,18 @@ export class TodosService {
     constructor(private http: HttpClient) {}
 
     addTodo(todo: Todo): Observable<Todo> {
-        return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo);
+        // const headers = new HttpHeaders({
+        //     'MyCustomHeader': Math.random().toString()
+        // })
+        return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo, {
+            headers: new HttpHeaders({
+                'MyCustomHeader': Math.random().toString()
+            })
+        });
     }
 
     fetchTodos(): Observable<Todo[]> {
-        return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todo2s?_limit=2')
+        return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
             .pipe(
                 delay(300),
                 catchError(error => {
