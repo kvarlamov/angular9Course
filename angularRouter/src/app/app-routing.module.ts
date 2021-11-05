5477e912e14686d96d7bc87b972eb78fb419ba0b
Konstantin Varlamov <konstantin.varlamov@arcadia.spb.ru>
2021-11-05 13:34:18 +0300

    BAsic router configuration

--- angularRouter/src/app/app-routing.module.ts
+++ angularRouter/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+//http://localhost:4200/ -> HomeComponent
+//http://localhost:4200/about -> AboutComponent
+//http://localhost:4200/posts -> PostsComponent
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'posts', component: PostsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
