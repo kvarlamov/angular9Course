b5100775747c7117a244c8ca22a58132f52eb853
Konstantin Varlamov <konstantin.varlamov@arcadia.spb.ru>
2021-11-06 16:45:14 +0300

    Children routing

--- angularRouter/src/app/app-routing.module.ts
+++ angularRouter/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutExtraComponent } from './about-extra/about-extra.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { PostComponent } from './post/post.component';
@@ -8,10 +9,13 @@ import { PostsComponent } from './posts/posts.component';
 //http://localhost:4200/ -> HomeComponent
 //http://localhost:4200/about -> AboutComponent
 //http://localhost:4200/posts -> PostsComponent
+//http://localhost:4200/about/extra
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
+  {path: 'about', component: AboutComponent, children: [
+    {path: 'extra', component: AboutExtraComponent}
+  ]},
   {path: 'posts', component: PostsComponent},
   {path: 'posts/:id', component: PostComponent}
 ];
